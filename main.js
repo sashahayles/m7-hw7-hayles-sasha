07d68b028f9ef84f701e400a68b8f8045397e7f0
Sasha Hayles <sashahayles@yahoo.com>
2019-10-08 20:49:32 -0400

    Attempt to fix JS error

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
+//var primHeader = document.getElementById ('welcome');
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var apiResult = JSON.parse(this.responseText);
+  if (this.readyState == 4 && this.status == 200) {
+    var apiResult = JSON.parse(this.responseText);
 
-        //Code Dealing With The API Data Goes Here
+    //Code Dealing With The API Data Goes Here
+    var userCity = document.createTextNode(apiResult.name);
+    var userWeather =document.createTextNode(apiResult.weather[0].description);
 
-    }
+    var weatherCity = document.getElementById('#Welcome');
+    var weatherTemp =document.getElementById('#current-weather');
+
+    //weatherCity.appendChild(userCity);
+    //  weatherTemp [1].appendChild(userWeather);
+
+
+
+
+
+
+
+    console.log (apiResult);
+    console.log (apiResult.name);
+    console.log (apiResult.weather);
+
+  }
 };
 xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=33578,us&appid=6efff70fe1477748e31c17d1c504635f', true);
 xmlhttp.send();
